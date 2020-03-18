74be01afbbd691c83549654920765633ac5630c8
Sheila R Moore <sherexmykes@github.com>
2020-03-17 20:55:18 -0400

    migrations

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,20 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/auth3-challenge.sqlite3'
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
+
 
   staging: {
     client: 'postgresql',
